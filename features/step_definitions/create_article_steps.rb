20225cbcdb6c9ab1b3b564b043a4c53fc7907f92
pedro_bras <pedro.c.bras@gmail.com>
2019-07-03 21:33:11 +0200

    adds root path to make 1st test pass, adding feature test for new article button that doesnt exist yet

--- features/step_definitions/create_article_steps.rb
+++ features/step_definitions/create_article_steps.rb
@@ -1,9 +1,9 @@
 Given("I visit the {string} page") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+    visit root_path
   end
   
-  When("I click {string} link") do |string|
-    pending # Write code here that turns the phrase above into concrete actions
+  When("I click {string} link") do |new_article|
+    click_on(new_article)
   end
   
   When("I fill in {string} with {string}") do |string, string2|
