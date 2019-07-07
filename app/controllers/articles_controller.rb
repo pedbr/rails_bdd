400b00180a2f1794daf9d05c2994a7fdc4599df1
pedro_bras <pedro.c.bras@gmail.com>
2019-07-08 00:06:12 +0200

    adds edit buttons to articles, feature working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,6 +24,16 @@ class ArticlesController < ApplicationController
       render 'new'
     end
   end
+
+  def update
+    @article = Article.find(params[:id])
+   
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
    
   private
     def article_params
